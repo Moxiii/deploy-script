869e2aba6ead557c8770cd0adbd9dbdbfd736fe3
Maxime <38160587+Moxiii@users.noreply.github.com>
2026-08-16 21:01:53 +0000

    up2date

--- deploys.sh
+++ deploys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 COMPOSE="docker compose -f compose.prod.yaml"
 PROJECT_PATH="$1"
 
@@ -10,26 +11,31 @@ fi
 
 cd "$PROJECT_PATH"
 
+echo "📦 Utilisation de compose.prod.yaml"
+
 echo "📦 Pull des dernières images..."
-docker compose pull || true
+$COMPOSE pull || true
 
 echo "🛑 Arrêt des anciens conteneurs..."
-docker compose down --remove-orphans
+$COMPOSE down --remove-orphans
 
 echo "🚀 Lancement des conteneurs..."
-docker compose up -d --build
+$COMPOSE up -d --build
 
 echo "⏳ Attente que tous les services deviennent 'healthy'..."
+
 MAX_WAIT=60
 i=0
+
 while [ $i -lt $MAX_WAIT ]; do
-  unhealthy=$($COMPOSE --filter "health=unhealthy" -q | wc -l)
-  starting=$($COMPOSE --filter "health=starting" -q | wc -l)
+
+  unhealthy=$($COMPOSE ps --filter "health=unhealthy" -q | wc -l)
+  starting=$($COMPOSE ps --filter "health=starting" -q | wc -l)
 
   if [ "$unhealthy" -eq 0 ] && [ "$starting" -eq 0 ]; then
     echo "✅ Tous les conteneurs sont 'healthy'."
 
-    echo "🧹 Nettoyage sécurisé des conteneurs/images/volumes inutilisés..."
+    echo "🧹 Nettoyage sécurisé..."
     docker container prune -f
     docker volume prune -f
     docker image prune -f
@@ -38,12 +44,15 @@ while [ $i -lt $MAX_WAIT ]; do
   fi
 
   echo "🕐 En attente... ($i/$MAX_WAIT sec)"
+
   sleep 2
   i=$((i + 2))
 done
+
 if [ $i -ge $MAX_WAIT ]; then
   echo "❌ Les conteneurs ne sont pas devenus sains dans les temps."
-  docker compose logs
+  $COMPOSE logs
   exit 1
 fi
+
 echo "✅ Déploiement terminé avec succès."
